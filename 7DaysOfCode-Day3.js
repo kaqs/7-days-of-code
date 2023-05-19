355166d8ad9548e545ff3d3e61236693a62155cc
karine <kaqsanfelice@gmail.com>
2023-05-19 11:36:58 +0100

    day 3 - add option to reset technologies question

--- 7DaysOfCode-Day3.js
+++ 7DaysOfCode-Day3.js
@@ -3,9 +3,9 @@ let area = prompt(`"Hello ${userName}! Welcome to the programming game! Do you w
 
 let language;
 
-if (area === "Front-End") {
+if (area.toLowerCase() === "front-end") {
     language = prompt("Would you like to learn React or Vue?");
-} else if (area === "Back-End") {
+} else if (area.toLowerCase() === "back-end") {
     language = prompt("Would you like to learn C# or Java?");
 } else {
     alert("Invalid Answer. Please enter 'Front-End' or 'Back-End'.");
@@ -14,34 +14,42 @@ if (area === "Front-End") {
 let question = prompt(`"Awesome! Would you like to specialise in ${area} or become a Fullstack developer?"`);
 
 
-if (question == "specialise") {
+if (question.toLowerCase() === "specialise") {
     alert(`Keep up with the good work in ${language}, ${area}`);
-} else if (question == "Fullstack") {
+} else if (question.toLowerCase() === "fullstack") {
     alert(`Great! You will be a well-rounded developer in no time`)
 } else {
     alert("Invalid answer. Please choose between specialise or Fullstack.")
     throw new Error("Invalid answer.")
 }
 
-let technologies = []; // Create an empty array
-let continueLearning = true;
-while (continueLearning) {
-    let newTech = prompt("What technology would you like to learn? (min of 3)");
-    const test = newTech.split(', ');
-    technologies = technologies.concat(test);
 
-    if (technologies.length < 3) {
-        alert(`You listed only ${technologies.length}. You still need to list ${3 - technologies.length} more.`);
-    }
+let repeat = "";
+do {
+    let technologies = []; // Create an empty array
+    let continueLearning = true;
 
-    if (technologies.length >= 3) {
-        let answer = prompt(`You listed ${technologies.length}. Is there any other technology you'd like to learn about? (yes or no)`);
-        if (answer === "no") {
-            continueLearning = false; // Stop the loop if answer is "no"
+    while (continueLearning) {
+        let newTech = prompt("What technology would you like to learn? (min of 3)");
+        const techList = newTech.split(', ');
+        technologies = technologies.concat(techList);
+
+        if (technologies.length < 3) {
+            alert(`You listed only ${technologies.length}. You still need to list ${3 - technologies.length} more.`);
+        }
+
+        if (technologies.length >= 3) {
+            let answer = prompt(`You listed ${technologies.length}. Is there any other technology you'd like to learn about? (yes or no)`);
+            if (answer === "no") {
+                continueLearning = false; // Stop the loop if answer is "no"
+            }
         }
     }
 
-}
+    alert("Congratulations on learning these new technologies: \n" + technologies.join("\n"));
 
-alert("Congratulations on learning these new technologies: \n" + technologies.join("\n"));
-
+    repeat = prompt("If you would like to repeat the questions please type 'Yes'. If you are happy with your answers please type 'No'");
+    if (repeat.toLowerCase() === "no") {
+        alert("Thank you for your time!");
+    }
+} while (repeat.toLowerCase() === "yes");
