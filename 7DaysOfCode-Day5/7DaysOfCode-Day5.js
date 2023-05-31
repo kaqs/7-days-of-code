3406d922cc4f6451a4c46783e01025a87d0a9d32
karine <kaqsanfelice@gmail.com>
2023-05-31 15:39:41 +0100

    day 5 - implemented categories and continueAddingChoice, general refactor

--- 7DaysOfCode-Day5/7DaysOfCode-Day5.js
+++ 7DaysOfCode-Day5/7DaysOfCode-Day5.js
@@ -1,31 +1,55 @@
 alert("Let's go shopping!");
-let addList;
-let addAgain;
+let productChoice;
+let continueAddingChoice;
 
 do {
-    let shoppingList = [];
+    let categoryShoppingList = [[], [], [], [], []];
+    let categories = ["1-Fruits & Veggies:", "2-Dairy & Meat:", "3-Frozen & Shelflife Food:", "4-Bakery & Snacks:", "5-Cleaning:"];
     let continueAdding = true;
 
     while (continueAdding) {
-        addList = prompt("What would you like to add to your shopping list?");
-        shoppingList.push(addList);
-        
-        addAgain = prompt("Would you like to add more items to your list? 1-Yes or 2-No");
-        
+        productChoice = prompt("What would you like to add to your shopping list?");
 
-        if (addAgain === "2") {
-            alert("Let's see your list then");
-            continueAdding = false;
-        }
+        let categoryChoice = prompt("What category would you like to add this item?\nChoose by the number:\n\n" + categories.join("\n"));
+        const categoryNumber = Number.parseInt(categoryChoice);
+        categoryShoppingList[categoryNumber - 1].push(productChoice);
+
+        do {
+
+            continueAddingChoice = prompt("Would you like to add more items to your list?\n\n1-Yes\n2-No");
+
+            if (continueAddingChoice != "1" && continueAddingChoice != "2") {
+                alert("Please enter a valid number");
+            } else if (continueAddingChoice === "2") {
+                alert("Let's see your list then");
+                continueAdding = false;
+            }
+        } while (continueAddingChoice != "1" && continueAddingChoice != "2")
     }
 
+    let myShoppingList = '';
+
+    for (let i = 0; i < categories.length; i++) {
+        myShoppingList = myShoppingList + categories[i] + "\n" + categoryShoppingList[i].join(", ") + "\n\n";
+    }
+
+    // different approach FOR statement
+    // let str = '';
+    // for (let i = 0; i < myCategories.length; i++) {
 
 
-} while (addAgain === "1")
+    //   let str2 = '';
+    //   for (let j = 0; j < shoppingList[i].length; j++) {
+    //     str2 = `${str2} \n\t ${shoppingList[i][j]}`;
+    //   }
 
 
+    //   str = `${str} \n ${myCategories[i]} ${str2}\n`;
+    // }
+
+    //alert(str);
 
 
+    alert("Here's your shopping list!:\n\n" + myShoppingList);
 
-//let categories = prompt ("What category would you like to add this item? \n Choose by the number:\n 1-Fruits & Veggies \n 2-Dairy & Meat \n 3-Frozen Food \n 4-Snacks & Sweets \n 5-Shelflife Food");
-
+} while (continueAddingChoice === "1")
