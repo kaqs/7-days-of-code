7d908b220ec38240fbbd7df4516e3dea3707257f
karine <kaqsanfelice@gmail.com>
2023-06-02 12:11:51 +0100

    day 6 - added 1st time logic, fixed remove products, added "not found", refactor

--- 7DaysOfCode-Day6/7DaysOfCode-Day6.js
+++ 7DaysOfCode-Day6/7DaysOfCode-Day6.js
@@ -1,61 +1,65 @@
 alert("Let's go shopping!");
-let productChoice;
+let firstTime = true;
 let nextStepChoice;
-let questionRemove;
-let showMyList;
-
-
+const categoryShoppingList = [[], [], [], [], []];
+const categoriesNames = ["1-Fruits & Veggies:", "2-Dairy & Meat:", "3-Frozen & Shelflife Food:", "4-Bakery, Drinks & Snacks:", "5-Cleaning & Misc:"];
+const removedProducts = [];
 
 do {
-    let categoryShoppingList = [[], [], [], [], []];
-    let categories = ["1-Fruits & Veggies:", "2-Dairy & Meat:", "3-Frozen & Shelflife Food:", "4-Bakery & Snacks:", "5-Cleaning & Misc:"];
-    let listRemoved = [];
-    
-    do {
-
+    if (firstTime == false) {
         nextStepChoice = Number.parseInt(prompt("What would you like to do now?\n\n1-Add\n2-Remove\n3-Show my list\n4-I'm done!"));
+    }
 
-        if (nextStepChoice === 1) {
-            productChoice = prompt("What would you like to add to your shopping list?");
+    if (nextStepChoice === 1 || firstTime) {
+        const productChoice = prompt("What would you like to add to your shopping list?");
 
-            let categoryChoice = prompt("What category would you like to add this item?\nChoose by the number:\n\n" + categories.join("\n"));
-            const categoryNumber = Number.parseInt(categoryChoice);
-            categoryShoppingList[categoryNumber - 1].push(productChoice);
+        const categoryChoice = prompt("What category would you like to add this item?\nChoose by the number:\n\n" + categoriesNames.join("\n"));
+        const categoryNumber = Number.parseInt(categoryChoice);
+        categoryShoppingList[categoryNumber - 1].push(productChoice);
 
-        } else if (nextStepChoice === 2) {
-            questionRemove = prompt("What item would you like to remove?");
+    } else if (nextStepChoice === 2) {
+        const removeChoice = prompt("What item would you like to remove?");
+        
+        const categoryChoice = prompt("What category would you like to remove this item?\nChoose by the number:\n\n" + categoriesNames.join("\n"));
+        const categoryNumber = Number.parseInt(categoryChoice);
+        const productShoppingList = categoryShoppingList[categoryNumber - 1];
+        const productIndex = productShoppingList.indexOf(removeChoice);
 
-            let categoryChoice = prompt("What category would you like to remove this item?\nChoose by the number:\n\n" + categories.join("\n"));
-            const categoryNumber = Number.parseInt(categoryChoice);
-            categoryShoppingList[categoryNumber - 1].splice(categories.indexOf(questionRemove), 1);
+        if(productIndex === -1){
+            alert ("404 - Item not found");
+        } else {
+            productShoppingList.splice(productIndex, 1);
+            removedProducts.push(removeChoice);
+        }        
 
-            listRemoved.push(questionRemove);
-            
-        } else if(nextStepChoice === 3){
-            showMyList = '';         
+    } else if (nextStepChoice === 3) {
+        let showMyList = '';
 
-            for (let i = 0; i < categories.length; i++) {
-                showMyList = showMyList + categories[i] + "\n" + categoryShoppingList[i].join(", ") + "\n\n";
-            }
-
-            alert ("Here's a preview of your shopping list!:\n\n" + showMyList + "Removed items:\n" + listRemoved.join("\n"));
-
-        } else if (nextStepChoice === 4) {
-            alert("Let's see your list then");
-            
-        } else if (nextStepChoice > 4) {
-            alert("Please enter a valid number");
+        for (let i = 0; i < categoriesNames.length; i++) {
+            showMyList = showMyList + categoriesNames[i] + "\n" + categoryShoppingList[i].join(", ") + "\n\n";
         }
 
+        alert("Here's a preview of your shopping list!:\n\n" + showMyList + "Removed items:\n" + removedProducts.join("\n"));
 
-    } while (nextStepChoice != 4)
+    } else if (nextStepChoice === 4) {
+        alert("Let's see your list then");
 
-
-    let myShoppingList = '';
-
-    for (let i = 0; i < categories.length; i++) {
-        myShoppingList = myShoppingList + categories[i] + "\n" + categoryShoppingList[i].join(", ") + "\n\n";
+    } else if (nextStepChoice > 4) {
+        alert("Please enter a valid number");
     }
+
+    firstTime = false;
+
+} while (nextStepChoice != 4)
+
+
+let myShoppingList = '';
+
+for (let i = 0; i < categoriesNames.length; i++) {
+    myShoppingList = myShoppingList + categoriesNames[i] + "\n" + categoryShoppingList[i].join(", ") + "\n\n";
+}
+
+alert("Here's your shopping list!:\n\n" + myShoppingList + "Removed items:\n" + removedProducts.join("\n"));
 
     // different approach FOR statement
     // let str = '';
@@ -74,6 +78,4 @@ do {
     //alert(str);
 
 
-    alert("Here's your shopping list!:\n\n" + myShoppingList + "Removed items:\n" + listRemoved.join("\n"));
 
-} while (nextStepChoice === "1")
